3a8c35644fb0710827c70000a06d07f4d61447b4
naren4488 <narendrakajla77@gmail.com>
2024-03-24 16:39:07 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Expense Tracker</h1>
+      </header>
+      <main>Expense Tracker Dashboard</main>
+    </div>
+  );
+}
+
+export default App;
